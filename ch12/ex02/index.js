2032828bc7856c0f63e246a1e7b22c01b7920a84
sho harashima <Sho.Harashima@jp.ricoh.com>
2024-06-18 16:58:07 +0900

    コメント追加

--- ch12/ex02/index.js
+++ ch12/ex02/index.js
@@ -6,9 +6,12 @@ export function fibonacciSequenceIterator() {
   let x = 0,
     y = 1;
   return {
+    // フィボナッチ数列の次の数値を生成する
     next: function () {
       const current = y;
+      // 次のフィボナッチ数を計算
       [x, y] = [y, x + y];
+      // 現在のフィボナッチ数とイテレーションの論理値を返す
       return { value: current, done: false };
     },
   };
--- ch12/ex03/index.js
+++ ch12/ex03/index.js
@@ -7,14 +7,19 @@
  * @yields {number} 次の整数
  */
 export function* counter() {
+  // カウンタの初期値0を代入
   let i = 0;
   while (true) {
     try {
+      // ジェネレータ関数の実行を一時停止し値を返す
       yield i;
     } catch (e) {
+      // エラーがキャッチされた場合はカウンタをリセット
       i = 0;
+      // ループの次のイテレーションに直接ジャンプ
       continue;
     }
+    // インクリメント
     i++;
   }
 }
