76b2a3184962c52bbaf1d8abfb7db0bcb92b9064
sho harashima <Sho.Harashima@jp.ricoh.com>
2024-11-22 09:21:17 +0900

    コミット

--- ch17/ex02/index.js
+++ ch17/ex02/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { createIssue, closeIssue, listOpenIssues } from './githubIssues.js';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { createIssue, closeIssue, listOpenIssues } from "./githubIssues.js";
 
 /**
  * メイン関数
@@ -11,69 +11,69 @@ function main() {
   const argv = yargs(hideBin(process.argv))
     // createコマンド: 新しいIssueを作成
     .command(
-      'create <repo> <title> [body]',
-      'Create a new issue',
+      "create <repo> <title> [body]",
+      "Create a new issue",
       (yargs) => {
         yargs
-          .positional('repo', {
-            describe: 'Repository in the format owner/repo', // リポジトリ形式
-            type: 'string',
+          .positional("repo", {
+            describe: "Repository in the format owner/repo", // リポジトリ形式
+            type: "string",
           })
-          .positional('title', {
-            describe: 'Title of the issue', // Issueのタイトル
-            type: 'string',
+          .positional("title", {
+            describe: "Title of the issue", // Issueのタイトル
+            type: "string",
           })
-          .positional('body', {
-            describe: 'Body of the issue', // Issueの本文
-            type: 'string',
-            default: '', // 本文はオプション
+          .positional("body", {
+            describe: "Body of the issue", // Issueの本文
+            type: "string",
+            default: "", // 本文はオプション
           });
       },
       async (argv) => {
         const data = await createIssue(argv.repo, argv.title, argv.body); // Issueを作成
         console.log(`Created issue: ${data.html_url}`); // 作成されたIssueのURLを出力
-      },
+      }
     )
     // closeコマンド: 指定したIssueをクローズ
     .command(
-      'close <repo> <issue_number>',
-      'Close an issue',
+      "close <repo> <issue_number>",
+      "Close an issue",
       (yargs) => {
         yargs
-          .positional('repo', {
-            describe: 'Repository in the format owner/repo', // リポジトリ形式
-            type: 'string',
+          .positional("repo", {
+            describe: "Repository in the format owner/repo", // リポジトリ形式
+            type: "string",
           })
-          .positional('issue_number', {
-            describe: 'Number of the issue to close', // クローズするIssueの番号
-            type: 'number',
+          .positional("issue_number", {
+            describe: "Number of the issue to close", // クローズするIssueの番号
+            type: "number",
           });
       },
       async (argv) => {
         const data = await closeIssue(argv.repo, argv.issue_number); // Issueをクローズ
         console.log(`Closed issue: ${data.html_url}`); // クローズされたIssueのURLを出力
-      },
+      }
     )
     // listコマンド: オープンなIssueの一覧を表示
     .command(
-      'list <repo>',
-      'List open issues',
+      "list <repo>",
+      "List open issues",
       (yargs) => {
-        yargs.positional('repo', {
-          describe: 'Repository in the format owner/repo', // リポジトリ形式
-          type: 'string',
+        yargs.positional("repo", {
+          describe: "Repository in the format owner/repo", // リポジトリ形式
+          type: "string",
         });
       },
       async (argv) => {
         const issues = await listOpenIssues(argv.repo); // オープンなIssueを取得
         if (issues.length === 0) {
-          console.log('No open issues found.');
+          console.log("No open issues found.");
         } else {
           issues.forEach((issue) => {
             console.log(`#${issue.number}: ${issue.title}`); // Issue番号とタイトルを表示
           });
         }
-      },
+      }
     )
     // ヘルプオプションを有効化
     .help().argv;
--- ch17/prettier.config.js
+++ ch17/prettier.config.js
@@ -1,6 +1,7 @@
 export default {
   semi: true, // セミコロンを使用
-  singleQuote: true, // シングルクォートを使用
-  trailingComma: 'all', // 末尾カンマを付与
+  singleQuote: false, // ダブルクォートを使用
+  trailingComma: "es5", // ES5の範囲で末尾カンマを付与
   tabWidth: 2, // インデント幅
+  printWidth: 80, // 行の長さ制限
 };
